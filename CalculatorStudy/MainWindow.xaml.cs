bde67fc8e9650e3f43e6ebf148ea3b61c14dd07f
OlegTinkoff-alphaBank <olegtinkoffizalphabank@gmail.com>
2026-03-17 19:03:44 +0300

    new

--- CalculatorStudy/MainWindow.xaml.cs
+++ CalculatorStudy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,7 +18,7 @@ namespace CalculatorStudy
     public partial class MainWindow : Window
     {
         double _firstNumber = 0;
-        string _operatot = "";
+        string _operator = "";
         bool _newinput = true;
         public MainWindow()
         {
@@ -32,7 +33,7 @@ namespace CalculatorStudy
                 case "AC":
                     MainDisplay.Text = "0";
                     ExpressionDisplay.Text = "";
-                    _operatot = "0";
+                    _operator = "0";
                     _firstNumber = 0;
                     _newinput = true;
                     break;
@@ -50,16 +51,34 @@ namespace CalculatorStudy
                     break;
                 case "+": case "-": case "×": case "÷":
                     _firstNumber =double.Parse(MainDisplay.Text);
-                    _operatot = input;
-                    ExpressionDisplay.Text = $"{_firstNumber} {_operatot}";
+                    _operator = input;
+                    ExpressionDisplay.Text = $"{_firstNumber} {_operator}";
                     _newinput = true;    
+                    break;
+                case "=":
+                    if (_operator == "") break;
+                    double second = double.Parse(MainDisplay.Text,
+                    CultureInfo.InvariantCulture);
+                    ExpressionDisplay.Text = $"{_firstNumber} {_operator} {second} =";
+                    double result = Calculate(_firstNumber, second, _operator);
+                    MainDisplay.Text = result.ToString(CultureInfo.InvariantCulture);
+                    _operator = "";
+                    _newinput = true;
+                    break;
+                case ".":
+                    if (_newinput) { MainDisplay.Text = "0."; _newinput = false; }
+                    else if (!MainDisplay.Text.Contains(".")) { MainDisplay.Text += "."; }
+                    break;
+                default:
+                    if(_newinput) { MainDisplay.Text = input; _newinput = false;}
+                    else MainDisplay.Text = MainDisplay.Text == "0"? input : MainDisplay.Text + input;
                     break;
 
 
             }
         }
 
-        private double calculate(double a, double b, string op) => op switch
+        private double Calculate(double a, double b, string op) => op switch
         {
             "+" => a + b,
             "-" => a - b,
